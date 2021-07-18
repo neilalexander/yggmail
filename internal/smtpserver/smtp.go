3c1e88a772c3a83c449ef180eafac2972faea6f4
Neil Alexander <neilalexander@users.noreply.github.com>
2021-07-18 23:03:28 +0100

    MPL 2.0

--- internal/smtpserver/smtp.go
+++ internal/smtpserver/smtp.go
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c) 2021 Neil Alexander
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package smtpserver
 
 import (
